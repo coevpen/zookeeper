2c5857d42cb26e89dc081d0af7da7653f74524fb
coevpen <koinuevins@gmail.com>
2022-01-09 21:13:25 -0600

    update server.js to allow addition of animals to json file

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 const {animals} = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// prase incoing JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -17,13 +25,14 @@ function filterByQuery(query, animalsArray) {
       }
       // Loop through each trait in the personalityTraits array:
       personalityTraitsArray.forEach(trait => {
-        // Check the trait against each animal in the filteredResults array.
-        // Remember, it is initially a copy of the animalsArray,
-        // but here we're updating it for each trait in the .forEach() loop.
-        // For each trait being targeted by the filter, the filteredResults
-        // array will then contain only the entries that contain the trait,
-        // so at the end we'll have an array of animals that have every one 
-        // of the traits when the .forEach() loop is finished.
+        /* Check the trait against each animal in the filteredResults array.
+        * Remember, it is initially a copy of the animalsArray,
+        * but here we're updating it for each trait in the .forEach() loop.
+        * For each trait being targeted by the filter, the filteredResults
+        * array will then contain only the entries that contain the trait,
+        * so at the end we'll have an array of animals that have every one 
+        * of the traits when the .forEach() loop is finished. 
+        */
         filteredResults = filteredResults.filter(
           animal => animal.personalityTraits.indexOf(trait) !== -1
         );
@@ -42,9 +51,41 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+// finds animal by id
 function findById(id, animalsArray){
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+// creates a new animal in the json file
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  // return finished code to post route for response
+  return body;
+}
+
+// validates incoming data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
 }
 
 app.get('/api/animals', (req,res) => {
@@ -65,6 +106,22 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
     
+});
+
+app.post('/api/animals', (req,res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  }
+  else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
